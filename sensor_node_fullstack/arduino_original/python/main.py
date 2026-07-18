e7f944e25a0786d36acee7cb1558a8d9af2acab1
Yuvraj Tech <yuvrajgotra35@gmail.com>
2026-07-19 04:34:30 +0530

    Enhance main.py with comments for better understanding
    Added comments for clarity and improved readability.

--- sensor_node_fullstack/arduino_original/python/main.py
+++ sensor_node_fullstack/arduino_original/python/main.py
@@ -1,26 +1,29 @@
-from arduino.app_utils import *
+# import all the lib
+from arduino.app_utils import *  
 import requests
 import time
 import math
 
+# UTL to urn the website localy
 FLASK_URL = "http://10.83.207.93:5000/detections"
 
 session = requests.Session()
 
-def loop():
-    try:
+def loop():  #  MAGIC
+    try:  # IF THERE IS NO ERROR THEN THIS WILL RUN 
         temperature = Bridge.call("get_temperature")
         humidity = Bridge.call("get_humidity")
         gas = Bridge.call("get_gas")
         distance = Bridge.call("get_distance")
         obstacle = Bridge.call("get_obstacle")
 
-        if math.isnan(temperature) or math.isnan(humidity):
+        if math.isnan(temperature) or math.isnan(humidity):  # IF TEMP AND HUMID IS NOT NUM THEN WE JUST EXIT FROM THERE
             print("Waiting for DHT22...")
             time.sleep(2)
             return
 
-        payload = {
+        #  PUT EVERY THING INT OBJ AND JUST PRINT THE VALUE
+        payload = {  
             "temperature": temperature,
             "humidity": humidity,
             "gas": gas,
